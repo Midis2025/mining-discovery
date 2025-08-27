1fef263ecaad76e74e40417676d7c7a08ea0a75e
Purujit Vij <midisofficial@gmail.com>
2025-08-27 05:44:16 +0530

    Update page.jsx
    new fixes

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,26 +1,7 @@
-import RichText from "@/components/richtext";
-import { getData } from "@/lib/getData";
+import React from 'react'
 
-export default async function AboutUs() {
-  const data = await getData("about-uses");
-  const description = data.data[0].description;
-
+export default function AbpoutUs() {
   return (
-    <main>
-      <section className="bg-black-bg">
-        <div className="container mx-auto px-4 py-4">
-          <h1 className="text-white text-2xl md:text-3xl font-bold mb-6">
-            About Us
-          </h1>
-          <p className="text-primary text-base md:text-lg leading-relaxed">
-            "Discover the Pulse of the Mining Industry: Your Go-To Source for
-            News, Insights, and Investment Opportunities"
-          </p>
-        </div>
-      </section>
-
-      {/* Main Content - About Us Description */}
-     
-    </main>
-  );
+    <div>AbpoutUs</div>
+  )
 }
